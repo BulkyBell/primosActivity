f53e939eb8c738358a75b3a40f44b5754f099f73
SergioColomer <sergio.scr43@gmail.com>
2023-10-18 20:38:10 +0200

    gb

--- app/src/main/java/com/example/primosactivity/MainActivity.kt
+++ app/src/main/java/com/example/primosactivity/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.primosactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val returnIntent = Intent()
+        returnIntent.putExtra("primos", primos.toArray())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var datos = data?.getIntArrayExtra("primos")
+        Log.d("Primos", datos.toString())
     }
 }
